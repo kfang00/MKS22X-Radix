31ccea3031e7a9baba3903586c8492b4f3471b8f
Kayla Fang <kfang00@stuy.edu>
2019-04-02 11:03:12 -0400

    started radixsort

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -233,6 +233,6 @@ public class MyLinkedList<E>{
     }
 
   public E removeFront() {
-    
+
   }
 }
--- Radix.java
+++ Radix.java
@@ -1,7 +1,30 @@
 public class Radix{
 
   public static void radixsort(int[]data){
-    
+    for (int a = 0; a < data.length; a++) {
+      int hold = data[0];
+      int count = 10;
+      @SuppressWarnings("unchecked")
+      MyLinkedList<Integer>[] buckets = new MyLinkedList[20];
+      for (int b = 0 ; b < buckets.length; b ++) {
+        buckets[b] = new MyLinkedList<Integer>();
+        buckets[b].add(data[a]);
+      }
+
+      if (hold < Math.abs(data[a])) {
+        hold = data[a];
+      }
+      //while ((hold / 10) != 0) {
+
+      //}
+System.out.println(buckets.toString());
+    }
+
+  }
+
+  public static void main(String[] args) {
+    int[] a = {34,56,3,46,36,3,6};
+    radixsort(a);
   }
 
 }
