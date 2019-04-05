6fff238ed187bc06b19836ca45f3e553fce70158
Kayla Fang <kfang00@stuy.edu>
2019-04-05 02:23:18 -0400

    changed approach - added another linkedlist

--- Radix.java
+++ Radix.java
@@ -2,33 +2,32 @@ public class Radix{
 
   public static void radixsort(int[]data){
     int hold = data[0];
-    int count = 0;
+    int count = 10;
     int idx = 0;
     @SuppressWarnings("unchecked")
-    MyLinkedList<Integer>[] buckets = new MyLinkedList[21];
+    MyLinkedList<Integer>[] buckets = new MyLinkedList[20];
     for (int b = 0 ; b < buckets.length; b++) {
       buckets[b] = new MyLinkedList<Integer>();
-      if (count < data.length) {
-      buckets[b].add(data[count]);
-      System.out.println(buckets[b].toString());
-      if (hold < Math.abs(data[count])) {
-        hold = data[count];
-      }
-      }
-      count++;
     }
-    count = 10;
+    MyLinkedList sorted = new MyLinkedList();
+    for (int i = 0 ; i < sorted.size(); i ++) {
+      buckets[0].add(data[i]);
+      System.out.println(buckets[0].toString());
+      if (hold < Math.abs(data[i])) {
+        hold = data[i];
+      }
+    }
     while ((hold / count) != 0) {
       for (int c = 0 ; c < buckets.length; c ++) {
 	if (buckets[c].size() > 0 ) {
 	  System.out.println(buckets[c].toString());
-	  buckets[20].extend(buckets[c]);
+	  sorted.extend(buckets[c]);
   	  //System.out.println(buckets[20].toString());
         }
       }
       //System.out.println(buckets[20].toString());
-      while (buckets[20].size() != 0) {
-        int h = buckets[20].removeFront();
+      while (sorted.size() != 0) {
+        int h = (int)sorted.removeFront();
 	if (h < 0) {
 	  buckets[9 + ((h % count) / (count / 10))].add(h);
           
